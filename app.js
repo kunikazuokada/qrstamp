bb0f061f39a8a293f9f112310287513afb4f95d7
Kunikazu Okada <okd@teppi.com>
2020-04-15 00:00:02 +0900

    jade eliminated

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.use('/', indexRouter);
+app.get('/', function(req,res){
+	const fs = require('fs');	
+	const appRoot = require('app-root-path');
+	res.sendFile(appRoot + '/public/idex.html');
+});
 
-var stampRouter = require('./routes/stamp');
+
+var stampRouter = require('./stamp');
 app.use('/stamp/', stampRouter);
 
 // catch 404 and forward to error handler
@@ -37,7 +39,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({message:"Error", status:err.status || 400});
 });
 
 module.exports = app;
